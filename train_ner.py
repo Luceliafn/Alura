3531907e56071d6f1bc7aab4d47570e52b21928f
Lucelia Vieira <luceliafn@gmail.com>
2021-08-22 18:24:37 -0300

    Create train_ner.py

---
+++ train_ner.py
@@ -0,0 +1,16 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1561\cocoasubrtf600
+{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 TRAIN_DATA = [\
+(\'93Almira \'e9 uma \'f3tima pessoa, gosto muito dela\'94, \{\'91entities\'92: [(0,6,\'94PER\'94)]\}),\
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+\cf0 (\'93Jo\'e3o foi para Bahia nas f\'e9rias\'94, \{\'91entities\'92:  [(14,19,\'94LOC\'94)], [(0,4,\'94PER\'94)]\}),\
+(\'93O rato \'e9 um animal muito forte\'94, \{\'91entities\'92: [(2,6,\'94ANIMAL\'94)]\}),\
+(\'93A data da festa foi 07/05/2018\'94, \{\'91entities\'92: [(20,30,\'94DATE\'94)]\}),\
+(\'93No dia 01/02/206 foi decretada a sentan\'e7a\'94, \{\'91entities\'92: [(7,17,\'94DATE\'94)]\})\
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+\cf0 ]}
